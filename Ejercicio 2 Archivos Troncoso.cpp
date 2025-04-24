90a921fd2285715e0db3f5aa8ce77eb7bd40c84e
TroncosoFernandoG <fernandogtroncoso@gmail.com>
2025-04-24 00:25:54 -0300

    Update Ejercicio 2 Archivos Troncoso.cpp

--- Ejercicio 2 Archivos Troncoso.cpp
+++ Ejercicio 2 Archivos Troncoso.cpp
@@ -1,21 +1,27 @@
 /*
-2) Modificar el programa anterior de manera que se puedan realizar búsquedas por Apellido y por nombre a la vez.
+2) Modificar el programa anterior de manera que se puedan realizar bÃºsquedas por Apellido y por nombre a la vez.
 
-Troncoso Fernando G 5°1°
+Troncoso Fernando G 5Â°1Â°
 */
 
 #include <stdio.h>
+#include <string.h>
+
 
 int main(){
 	int op;
+	
+	char Lector[200];
+	FILE *dp;
+
 do{
 	
 	
 	printf("\n\n\n\t\t--Elegir una de las Opciones--\t\t");
 	
-	printf("\n\n\nIngresar Datos Personales\t\t");
-	printf("Busqueda de Datos\n\n\n");
-	printf("Salir del Programa\n\n\n");
+	printf("\n\n\n1-Ingresar Datos Personales\t\t");
+	printf("2-Busqueda de Datos\n\n\n");
+	printf("3-Salir del Programa\n\n\n");
 	
 	
 	scanf("%d", &op);
@@ -25,21 +31,75 @@ do{
 
 		do{
 			printf("Opcion no valida, por favor intentar nuevamente\n");
-			scanf("%d", &op);
+				scanf("%d", &op);
 		}while(op<1||op>3);
 	}
 	
+	getchar();
+	
 	switch(op)
 	{
-		case 1:{
-			printf("tu\n");
+		case 1:
+			char nombre[50];
+			char apellido[50];
+			int DNI;
+			
+			dp=fopen("DatosPersonales2.txt", "a");
+			
+	    	printf("Ingrese el Nombre\n");
+				fgets(nombre, sizeof(nombre), stdin);
+				nombre[strcspn(nombre, "\n")]=0;
+			printf("Ingrese el Apellido\n");
+				fgets(apellido, sizeof(apellido), stdin);
+				apellido[strcspn(apellido, "\n")]=0;
+			printf("Ingrese el DNI\n");
+				scanf("%d", &DNI);
+		
+			fprintf(dp,"%s %s %d\n", nombre, apellido, DNI);
+				
+			fclose(dp);
+			
 			break;
-		}
-		case 2:{
-			printf("vie\n");
-			break;
-		}	
+		
+		case 2:
+			char nombrecomp[50];
+			char apellidocomp[50];
+			
+			
+			printf("Ingresar nombre y apellido para buscar el respectivo dato:  ");
+               printf("\n nombre:");
+			    fgets(nombrecomp, sizeof(nombrecomp), stdin);
+				nombrecomp[strcspn(nombrecomp, "\n")]=0;
+			   printf(" apellido:");
+			    fgets(apellidocomp, sizeof(apellidocomp), stdin);
+				apellidocomp[strcspn(apellidocomp, "\n")]=0;
+                
+
+                dp=fopen("DatosPersonales2.txt", "r");
+                int igual=0;
+            
+		while(fgets(Lector, sizeof(Lector), dp) != NULL){
+		
+			
+                    if (sscanf(Lector, "%s %s %d\n", nombre, apellido, &DNI)) {
+                        if (strcmp(nombre, nombrecomp) == 0 && strcmp(apellido, apellidocomp) == 0) {
+                            printf("Su DNI es: %d\n", DNI);
+                        	igual=1;
+                        break;
+                        }
+                    }
+                }
+			
+			
+                fclose(dp);
+                if (!igual) {
+                        printf("No se encontro el DNI para la persona llamada %s %s.\n", nombrecomp, apellidocomp);
+                }
+
+                break;
+		
 	}	
+	
 	
 }while(op!=3);
 	
